ed778f7caae88bbbfcf44dbe47a6220a8165beae
luiggy macias <ferrinluiggy@gmail.com>
2022-05-26 14:33:55 -0500

    fdffd

--- src/03-callbacks-and-events/02-async-cps/index.ts
+++ src/03-callbacks-and-events/02-async-cps/index.ts
@@ -7,3 +7,12 @@ function additionAsync(a: number, b: number, callback: (result: number) => void)
 console.log('before');
 additionAsync(1, 2, (result) => console.log(`Result: ${result}`));
 console.log('after');
+
+let a
+console.log('before', a);
+additionAsync(1, 2, (result) => {
+  a= result
+  console.log('during', a);
+  return a
+});
+console.log('after', a);
