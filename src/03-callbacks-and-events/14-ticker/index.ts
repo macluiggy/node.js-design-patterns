3e81a46e94a3180f6f243d6c5e734e3917e21a5a
luiggy macias <ferrinluiggy@gmail.com>
2022-05-30 16:38:05 -0500

    ffdd

--- src/03-callbacks-and-events/14-ticker/index.ts
+++ src/03-callbacks-and-events/14-ticker/index.ts
@@ -3,15 +3,15 @@ import EventEmitter from "events";
 function ticker(limit: number | undefined, callback: Function) {
   const emitter = new EventEmitter();
   // emitter.emit("tick", limit);
-  let e = 0
+  let e = 0;
   let i = setInterval(() => {
-    emitter.emit('tick')
-    e++
+    emitter.emit("tick");
+    e++;
     if (Date.now() % 5 === 0) {
-      emitter.emit('error', 'Event: timestamp is divisible by 5')
-      return callback('Callback: timestamp is divisible by 5')
+      emitter.emit("error", "Event: timestamp is divisible by 5");
+      return callback("Callback: timestamp is divisible by 5");
     }
-  }, 50)
+  }, 50);
   setTimeout(() => {
     clearInterval(i);
     callback(null, `the event has been emitted ${e} times`);
@@ -19,11 +19,14 @@ function ticker(limit: number | undefined, callback: Function) {
   return emitter;
 }
 
-ticker(1000, ((err, msg) => {
+ticker(1000, (err, msg) => {
   if (err) {
     console.error(err);
     return;
   }
   console.log(msg);
-})).on("tick", () => console.log("tick"))
-.on('error', (err) => console.error(err))
+})
+  .on("tick", () => console.log("tick"))
+  .on("error", (err) => console.error(err))
+  // .removeListener("tick", () => console.log("tick"))
+  // .on("tick", () => console.log("oiesamamada"));
