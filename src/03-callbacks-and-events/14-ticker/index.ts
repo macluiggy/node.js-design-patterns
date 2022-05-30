02cf2248ce0bf0ea9c1c9ccac53272b56669d014
luiggy macias <ferrinluiggy@gmail.com>
2022-05-30 15:23:09 -0500

    chapter 3 finished

--- src/03-callbacks-and-events/14-ticker/index.ts
+++ src/03-callbacks-and-events/14-ticker/index.ts
@@ -1,15 +1,29 @@
 import EventEmitter from "events";
 
-function ticker(limit?: number | undefined, callback?: () => void) {
-  let delay = 50
-  let i = 1
+function ticker(limit: number | undefined, callback: Function) {
   const emitter = new EventEmitter();
-  let interval = setInterval(() => {
-    if (i > delay) clearInterval(interval)
-    emitter.emit("tick")
-    i++
-  },  50)
+  // emitter.emit("tick", limit);
+  let e = 0
+  let i = setInterval(() => {
+    emitter.emit('tick')
+    e++
+    if (Date.now() % 5 === 0) {
+      emitter.emit('error', 'Event: timestamp is divisible by 5')
+      return callback('Callback: timestamp is divisible by 5')
+    }
+  }, 50)
+  setTimeout(() => {
+    clearInterval(i);
+    callback(null, `the event has been emitted ${e} times`);
+  }, limit);
   return emitter;
 }
 
-ticker(100).on("tick", () => console.log("tick"));
+ticker(1000, ((err, msg) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  console.log(msg);
+})).on("tick", () => console.log("tick"))
+.on('error', (err) => console.error(err))
